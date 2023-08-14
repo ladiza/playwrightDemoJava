2fe4b8cb55ec923fc893b4a2c821d4b1bddf026d
Ladislav Zahnas <ladizala@gmail.com>
2023-08-14 23:09:21 +0200

    Add first test to App.java

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+
+import java.nio.file.Paths;
+
+public class App {
+  public static void main(String[] args) {
+    try (Playwright playwright = Playwright.create()) {
+      Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+      Page page = browser.newPage();
+      page.navigate("http://playwright.dev");
+      System.out.println(page.title());
+      page.pause();
+      page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+    }
+  }
+}
